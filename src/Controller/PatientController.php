c6245f02f325d9647a1f0e91821025034c4978a5
cheikh tourad <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-05-23 15:43:41 +0100

    home page for patient

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -8,11 +8,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class PatientController extends AbstractController
 {
-    #[Route('/patient', name: 'app_patient')]
+    #[Route('/patient', name: 'app_home_patient')]
     public function index(): Response
     {
         return $this->render('patient/index.html.twig', [
-            'controller_name' => 'PatientController',
+        ]);
+    }
+    #[Route('/patient/profile', name: 'app_profile_patient')]
+    public function profil(): Response
+    {
+        return $this->render('patient/index.html.twig', [
+        ]);
+    }
+    #[Route('/patient/post', name: 'app_post_patient')]
+    public function post(): Response
+    {
+        return $this->render('patient/index.html.twig', [
         ]);
     }
 }
